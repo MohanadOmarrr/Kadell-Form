97404c4354cf447938278d7f5a87b40127221846
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-07-30 22:02:38 +0200

    file.txt

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.utils.datastructures import MultiValueDictKeyError
 
 
 def homepage(request):
-
-    try:
-        age = request.GET['age']
-        return render(request, 'index.html', {"age": age})
-
-    except MultiValueDictKeyError:
-        pass
-
     return render(request, 'index.html')
--- survey/views.py
+++ survey/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 
 
 # Create your views here.
 def survey(request):
+    try:
+        all_answers = [request.GET['q1'], request.GET['q2'], request.GET['q3'], request.GET['q4'], request.GET['q5'],
+                       request.GET['q6'], request.GET['q7'], request.GET['q8'], request.GET['q9'], request.GET['q10'],
+                       request.GET['q11']]
+
+        with open("file.txt", 'w') as datafile:
+            for answer in all_answers:
+                datafile.writelines(f"{answer}\n")
+
+        return render(request, 'index.html', {"all_answers": all_answers})
+
+    except MultiValueDictKeyError:
+        pass
+
     return render(request, 'survey.html')
