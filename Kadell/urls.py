a570cdfb807892eee19b2d44b83c2505aa52687d
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-03 23:36:33 +0200

    static files

--- Kadell/urls.py
+++ Kadell/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from Kadell import settings
 from survey import views
 
 urlpatterns = [
@@ -24,4 +22,4 @@ urlpatterns = [
     path('submit/', views.submit, name="submit"),
     path('done/', views.done, name="done"),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
