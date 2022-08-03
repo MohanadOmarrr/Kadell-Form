cc77919a49e960145da6524d070d4e2b5a7a18ec
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-03 21:09:40 +0200

    static files

--- Kadell/settings.py
+++ Kadell/settings.py
@@ -129,7 +129,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 
 
