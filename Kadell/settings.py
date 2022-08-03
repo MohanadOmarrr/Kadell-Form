1ef9ce2dcad517fb0596872cb0a85bcbd47a967f
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-03 23:37:57 +0200

    static files

--- Kadell/settings.py
+++ Kadell/settings.py
@@ -124,9 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
