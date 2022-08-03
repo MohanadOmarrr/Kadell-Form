4ffe9207c34354760f6ddc939b7b920f5c0a33ef
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-03 22:00:07 +0200

    static files

--- Kadell/settings.py
+++ Kadell/settings.py
@@ -131,13 +131,13 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
