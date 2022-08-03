51442068691804e4f5c6334472a43bf32f56ceae
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-03 21:12:34 +0200

    static files

--- Kadell/settings.py
+++ Kadell/settings.py
@@ -121,15 +121,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Kadell/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
 
 
 
