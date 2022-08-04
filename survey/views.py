bc5750965a7c563199b0616a4857dceb40bc589b
MohanadOmarrr <102043745+MohanadOmarrr@users.noreply.github.com>
2022-08-04 13:48:04 +0200

    static files

--- survey/views.py
+++ survey/views.py
@@ -11,23 +11,6 @@ rawan_acc = 'rawaneldalie@gmail.com'
 
 # =============================================================================
 
-def uploadImage():
-    # Upload the image and get its URL
-    # ==============================
-
-    # Upload the image.
-    # Set the asset's public ID and allow overwriting the asset with new versions
-    cloudinary.uploader.upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/butterfly.jpeg",
-                               public_id="quickstart_butterfly", unique_filename=False, overwrite=True)
-
-    # Build the URL for the image and save it in the variable 'srcURL'
-    srcURL = cloudinary.CloudinaryImage("quickstart_butterfly").build_url()
-
-    # Log the image URL to the console.
-    # Copy this URL in a browser tab to generate the image on the fly.
-    print("****2. Upload an image****\nDelivery URL: ", srcURL, "\n")
-uploadImage()
-
 
 # =============================================================================
 # SEND EMAIL FUNCTION
